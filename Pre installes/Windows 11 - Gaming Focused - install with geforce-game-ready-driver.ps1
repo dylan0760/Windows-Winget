bfc7e51bd323f736ab815815c44a60b4541f78d0
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2024-06-26 15:51:20 +0100

    Update Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1

--- Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
+++ Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
@@ -1,130 +1,95 @@
 ﻿function Check-Update-Winget {
-    $logFilePath = "$ENV:TEMP\wingetLog.txt"
-    try {
-        # Clear log file
-        Clear-Content -Path $logFilePath -ErrorAction SilentlyContinue
-    } catch {}
-
-    function Log-Message {
-        param ($message)
-        $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        Add-Content -Path $logFilePath -Value "[$timestamp] $message"
-    }
-
-    # Start logging
-    Log-Message "Starting Winget update check."
-
+    # Check if Winget is installed
     $wingetExists = $true
     try {
         $wingetVersionFull = winget --version
-        Log-Message "Winget version found: $wingetVersionFull"
     } catch [System.Management.Automation.CommandNotFoundException], [System.Management.Automation.ApplicationFailedException] {
-        Log-Message "Winget was not found."
+        Write-Warning "Winget was not found"
         $wingetExists = $false
     } catch {
-        Log-Message "Winget was not found due to unknown reasons."
+        Write-Warning "Winget was not found due to unknown reasons"
         $wingetExists = $false
     }
 
     if ($wingetExists) {
         # Check if Preview Version
-        if ($wingetVersionFull -contains "-preview") {
-            $wingetVersion = $wingetVersionFull.Trim("preview")
+        if ($wingetVersionFull.Contains("-preview")) {
+            $wingetVersion = $wingetVersionFull.Trim("-preview")
             $wingetPreview = $true
-            Log-Message "Winget is a preview version: $wingetVersion"
         } else {
             $wingetVersion = $wingetVersionFull
             $wingetPreview = $false
-            Log-Message "Winget is a release version: $wingetVersion"
         }
-
+        
         $wingetCurrentVersion = [System.Version]::Parse($wingetVersion.Trim('v'))
-        Log-Message "Parsed current Winget version: $wingetCurrentVersion"
-
+        
         # Grabs the latest release of Winget from the Github API for version check process.
         $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/Winget-cli/releases/latest" -Method Get -ErrorAction Stop
-        $wingetLatestVersion = [System.Version]::Parse(($response.tag_name).Trim('v')) # Stores version number of latest release.
+        $wingetLatestVersion = [System.Version]::Parse(($response.tag_name).Trim('v')) #Stores version number of latest release.
         $wingetOutdated = $wingetCurrentVersion -lt $wingetLatestVersion
-
+        
         if (!$wingetOutdated) {
-            Log-Message "Winget is up to date (Version: $wingetVersionFull)"
+            Write-Host "Winget is up to date (Version: $wingetVersionFull)" -ForegroundColor Green
         } else {
-            Log-Message "Winget is outdated (Current Version: $wingetVersionFull, Latest Version: $wingetLatestVersion)"
+            Write-Host "Winget is outdated (Current Version: $wingetVersionFull, Latest Version: $wingetLatestVersion)" -ForegroundColor Yellow
             # Code to update Winget
-            Log-Message "Downloading Winget Prerequisites."
+
+            Write-Host "Downloading Winget Prerequisites`n"
             Get-WinUtilWingetPrerequisites
-            Log-Message "Downloading Winget and License File."
+            Write-Host "Downloading Winget and License File`r"
             Get-WinUtilWingetLatest
-
-            Log-Message "Installing Winget with Prerequisites."
+            Write-Host "Installing Winget w/ Prerequisites`r"
             Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
-
-            Log-Message "Manually adding Winget Sources from Winget CDN."
+            Write-Host "Manually adding Winget Sources, from Winget CDN."
             Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix
-            Log-Message "Winget Installed."
+            Write-Host "Winget Installed" -ForegroundColor Green
 
             # Enabling NuGet and Module...
-            Log-Message "Enabling NuGet and Module."
+            Write-Host "Enabling NuGet and Module..."
             Install-PackageProvider -Name NuGet -Force
             Install-Module -Name Microsoft.WinGet.Client -Force
 
             # Refreshing Environment Variables
-            Log-Message "Refreshing Environment Variables."
+            Write-Output "Refreshing Environment Variables...`n"
             $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
         }
     } else {
-        Log-Message "Winget is not installed. Proceeding with installation."
+        Write-Host "Winget is not installed. Proceeding with installation."
 
         # Code to install Winget if it's not installed
 
-        Log-Message "Downloading Winget Prerequisites."
+        Write-Host "Downloading Winget Prerequisites`n"
         Get-WinUtilWingetPrerequisites
-        Log-Message "Downloading Winget and License File."
+        Write-Host "Downloading Winget and License File`r"
         Get-WinUtilWingetLatest
-
-        Log-Message "Installing Winget with Prerequisites."
+        Write-Host "Installing Winget w/ Prerequisites`r"
         Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
-
-        Log-Message "Manually adding Winget Sources from Winget CDN."
+        Write-Host "Manually adding Winget Sources, from Winget CDN."
         Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix
-        Log-Message "Winget Installed."
+        Write-Host "Winget Installed" -ForegroundColor Green
 
         # Enabling NuGet and Module...
-        Log-Message "Enabling NuGet and Module."
+        Write-Host "Enabling NuGet and Module..."
         Install-PackageProvider -Name NuGet -Force
         Install-Module -Name Microsoft.WinGet.Client -Force
 
         # Refreshing Environment Variables
-        Log-Message "Refreshing Environment Variables."
+        Write-Output "Refreshing Environment Variables...`n"
         $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     }
-
-    Log-Message "Installing packages."
-
-    $packages = @(
-        "Brave.Brave",
-        "Discord.Discord",
-        "Valve.Steam",
-        "ElectronicArts.EADesktop",
-        "7zip.7zip",
-        "Notepad++.Notepad++",
-        "Ubisoft.Connect",
-        "EpicGames.EpicGamesLauncher",
-        "GOG.Galaxy",
-        "Nvidia.GeForceExperience"
-    )
-
-    foreach ($package in $packages) {
-        try {
-            Log-Message "Installing $package."
-            winget install --id=$package -e --accept-package-agreements --accept-source-agreements --silent
-        } catch {
-            Log-Message "Failed to install $package: $_"
-        }
-    }
-
-    Log-Message "Finished installing packages."
+	{
+winget install --id=Brave.Brave -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=Discord.Discord -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=Valve.Steam -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=ElectronicArts.EADesktop -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=7zip.7zip -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=Notepad++.Notepad++ -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=Ubisoft.Connect -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=EpicGames.EpicGamesLauncher -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=GOG.Galaxy -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id=Nvidia.GeForceExperience -e --accept-package-agreements --accept-source-agreements --silent
+}
 }
 
-# Run the function
-Check-Update-Winget
+
+
