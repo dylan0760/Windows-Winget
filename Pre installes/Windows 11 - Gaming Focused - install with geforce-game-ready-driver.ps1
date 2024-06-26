b7f83fbebf93f75b3a912aa2ccdfab61b270d929
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2024-06-26 12:47:30 +0100

    update

---
+++ Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
@@ -0,0 +1,92 @@
+﻿function Check-Update-Winget {
+    # Check if Winget is installed
+    $wingetExists = $true
+    try {
+        $wingetVersionFull = winget --version
+    } catch [System.Management.Automation.CommandNotFoundException], [System.Management.Automation.ApplicationFailedException] {
+        Write-Warning "Winget was not found"
+        $wingetExists = $false
+    } catch {
+        Write-Warning "Winget was not found due to unknown reasons"
+        $wingetExists = $false
+    }
+
+    if ($wingetExists) {
+        # Check if Preview Version
+        if ($wingetVersionFull.Contains("-preview")) {
+            $wingetVersion = $wingetVersionFull.Trim("-preview")
+            $wingetPreview = $true
+        } else {
+            $wingetVersion = $wingetVersionFull
+            $wingetPreview = $false
+        }
+        
+        $wingetCurrentVersion = [System.Version]::Parse($wingetVersion.Trim('v'))
+        
+        # Grabs the latest release of Winget from the Github API for version check process.
+        $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/Winget-cli/releases/latest" -Method Get -ErrorAction Stop
+        $wingetLatestVersion = [System.Version]::Parse(($response.tag_name).Trim('v')) #Stores version number of latest release.
+        $wingetOutdated = $wingetCurrentVersion -lt $wingetLatestVersion
+        
+        if (!$wingetOutdated) {
+            Write-Host "Winget is up to date (Version: $wingetVersionFull)" -ForegroundColor Green
+        } else {
+            Write-Host "Winget is outdated (Current Version: $wingetVersionFull, Latest Version: $wingetLatestVersion)" -ForegroundColor Yellow
+            # Code to update Winget
+
+            Write-Host "Downloading Winget Prerequisites`n"
+            Get-WinUtilWingetPrerequisites
+            Write-Host "Downloading Winget and License File`r"
+            Get-WinUtilWingetLatest
+            Write-Host "Installing Winget w/ Prerequisites`r"
+            Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
+            Write-Host "Manually adding Winget Sources, from Winget CDN."
+            Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix
+            Write-Host "Winget Installed" -ForegroundColor Green
+
+            # Enabling NuGet and Module...
+            Write-Host "Enabling NuGet and Module..."
+            Install-PackageProvider -Name NuGet -Force
+            Install-Module -Name Microsoft.WinGet.Client -Force
+
+            # Refreshing Environment Variables
+            Write-Output "Refreshing Environment Variables...`n"
+            $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+        }
+    } else {
+        Write-Host "Winget is not installed. Proceeding with installation."
+
+        # Code to install Winget if it's not installed
+
+        Write-Host "Downloading Winget Prerequisites`n"
+        Get-WinUtilWingetPrerequisites
+        Write-Host "Downloading Winget and License File`r"
+        Get-WinUtilWingetLatest
+        Write-Host "Installing Winget w/ Prerequisites`r"
+        Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
+        Write-Host "Manually adding Winget Sources, from Winget CDN."
+        Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix
+        Write-Host "Winget Installed" -ForegroundColor Green
+
+        # Enabling NuGet and Module...
+        Write-Host "Enabling NuGet and Module..."
+        Install-PackageProvider -Name NuGet -Force
+        Install-Module -Name Microsoft.WinGet.Client -Force
+
+        # Refreshing Environment Variables
+        Write-Output "Refreshing Environment Variables...`n"
+        $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    }
+}
+
+Check-Update-Winget
+
+winget install --id=Brave.Brave -e --accept-package-agreements --accept-source-agreements --silent
+winget install -e --id Discord.Discord --accept-package-agreements --accept-source-agreement
+winget install -e --id Valve.Steam --accept-package-agreements --accept-source-agreements
+winget install -e --id ElectronicArts.EADesktop --accept-package-agreements --accept-source-agreements
+winget install -e --id 7zip.7zip --accept-package-agreements --accept-source-agreements
+winget install -e --id Notepad++.Notepad++ --accept-package-agreements --accept-source-agreements
+winget install -e --id Ubisoft.Connect --accept-package-agreements --accept-source-agreements
+winget install -e --id EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements
+winget install -e --id Nvidia.GeForceExperience --accept-package-agreements --accept-source-agreements
