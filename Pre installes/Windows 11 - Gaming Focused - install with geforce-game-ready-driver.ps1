cf45e41a96c5683ca2cd99b4aeadadbfa4b139b3
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2024-06-26 15:34:06 +0100

    Update Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1

--- Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
+++ Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
@@ -78,7 +78,16 @@
         $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     }
 	{
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/dylan0760/Windows-Winget/main/Pre%20installes/Windows%2011%20-%20Gaming%20Focused%20-%20install%20with%20geforce-game-ready-driver%20Programs.ps1")
+winget install --id=Brave.Brave -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id Discord.Discord -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id Valve.Steam -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id ElectronicArts.EADesktop -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id 7zip.7zip -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id Notepad++.Notepad++ -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id Ubisoft.Connect -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id EpicGames.EpicGamesLauncher -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id GOG.Galaxy -e --accept-package-agreements --accept-source-agreements --silent
+winget install --id Nvidia.GeForceExperience -e --accept-package-agreements --accept-source-agreements --silent
 }
 }
 
