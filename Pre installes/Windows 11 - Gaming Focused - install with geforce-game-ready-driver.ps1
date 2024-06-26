210ba3563d04b77f6060171d904b05cbb769c867
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2024-06-26 14:29:12 +0100

    Update Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1

--- Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
+++ Pre installes/Windows 11 - Gaming Focused - install with geforce-game-ready-driver.ps1
@@ -78,14 +78,14 @@
         $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 		
 		winget install --id=Brave.Brave -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id Discord.Discord -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id Valve.Steam -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id ElectronicArts.EADesktop -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id 7zip.7zip -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id Notepad++.Notepad++ -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id Ubisoft.Connect -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id EpicGames.EpicGamesLauncher -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id GOG.Galaxy -e --accept-package-agreements --accept-source-agreements --silent
-winget install --id Nvidia.GeForceExperience -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id Discord.Discord -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id Valve.Steam -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id ElectronicArts.EADesktop -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id 7zip.7zip -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id Notepad++.Notepad++ -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id Ubisoft.Connect -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id EpicGames.EpicGamesLauncher -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id GOG.Galaxy -e --accept-package-agreements --accept-source-agreements --silent
+		winget install --id Nvidia.GeForceExperience -e --accept-package-agreements --accept-source-agreements --silent
     }
 }
