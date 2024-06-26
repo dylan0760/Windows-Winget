751fd849ff0166ae0942345c017afe89d113eb31
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2024-06-26 12:20:49 +0100

    Public

--- Winget Install/Windows Winget Install.ps1
+++ Winget Install/Windows Winget Install.ps1
@@ -1,40 +1,82 @@
-# Set the PSGallery repository as trusted to avoid prompts
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+﻿function Check-Update-Winget {
+    # Check if Winget is installed
+    $wingetExists = $true
+    try {
+        $wingetVersionFull = winget --version
+    } catch [System.Management.Automation.CommandNotFoundException], [System.Management.Automation.ApplicationFailedException] {
+        Write-Warning "Winget was not found"
+        $wingetExists = $false
+    } catch {
+        Write-Warning "Winget was not found due to unknown reasons"
+        $wingetExists = $false
+    }
 
-# Install the winget-install script from the PSGallery repository
-Install-Script -Name winget-install -Force
+    if ($wingetExists) {
+        # Check if Preview Version
+        if ($wingetVersionFull.Contains("-preview")) {
+            $wingetVersion = $wingetVersionFull.Trim("-preview")
+            $wingetPreview = $true
+        } else {
+            $wingetVersion = $wingetVersionFull
+            $wingetPreview = $false
+        }
+        
+        $wingetCurrentVersion = [System.Version]::Parse($wingetVersion.Trim('v'))
+        
+        # Grabs the latest release of Winget from the Github API for version check process.
+        $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/Winget-cli/releases/latest" -Method Get -ErrorAction Stop
+        $wingetLatestVersion = [System.Version]::Parse(($response.tag_name).Trim('v')) #Stores version number of latest release.
+        $wingetOutdated = $wingetCurrentVersion -lt $wingetLatestVersion
+        
+        if (!$wingetOutdated) {
+            Write-Host "Winget is up to date (Version: $wingetVersionFull)" -ForegroundColor Green
+        } else {
+            Write-Host "Winget is outdated (Current Version: $wingetVersionFull, Latest Version: $wingetLatestVersion)" -ForegroundColor Yellow
+            # Code to update Winget
 
+            Write-Host "Downloading Winget Prerequisites`n"
+            Get-WinUtilWingetPrerequisites
+            Write-Host "Downloading Winget and License File`r"
+            Get-WinUtilWingetLatest
+            Write-Host "Installing Winget w/ Prerequisites`r"
+            Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
+            Write-Host "Manually adding Winget Sources, from Winget CDN."
+            Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix
+            Write-Host "Winget Installed" -ForegroundColor Green
 
-# Run the winget-install script
-winget-install.ps1
+            # Enabling NuGet and Module...
+            Write-Host "Enabling NuGet and Module..."
+            Install-PackageProvider -Name NuGet -Force
+            Install-Module -Name Microsoft.WinGet.Client -Force
 
+            # Refreshing Environment Variables
+            Write-Output "Refreshing Environment Variables...`n"
+            $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+        }
+    } else {
+        Write-Host "Winget is not installed. Proceeding with installation."
 
+        # Code to install Winget if it's not installed
 
-# This script will download and install the latest version of App Installer from GitHub
+        Write-Host "Downloading Winget Prerequisites`n"
+        Get-WinUtilWingetPrerequisites
+        Write-Host "Downloading Winget and License File`r"
+        Get-WinUtilWingetLatest
+        Write-Host "Installing Winget w/ Prerequisites`r"
+        Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
+        Write-Host "Manually adding Winget Sources, from Winget CDN."
+        Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix
+        Write-Host "Winget Installed" -ForegroundColor Green
 
-# Define the download URL and the output file path
-$download_url = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-$output_file = "C:\Temp\WinGet.msixbundle"
+        # Enabling NuGet and Module...
+        Write-Host "Enabling NuGet and Module..."
+        Install-PackageProvider -Name NuGet -Force
+        Install-Module -Name Microsoft.WinGet.Client -Force
 
-# Make new folder
-New-Item -Path C:\ -Name Temp -ItemType Directory
+        # Refreshing Environment Variables
+        Write-Output "Refreshing Environment Variables...`n"
+        $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    }
+}
 
-
-# Save the original value of the progress preference variable
-$originalProgressPreference = $ProgressPreference
-
-# Set the progress preference variable to SilentlyContinue
-$ProgressPreference = 'SilentlyContinue'
-
-
-# Download the App Installer file
-Invoke-WebRequest -Uri $download_url -OutFile $output_file
-
-# Restore the original value of the progress preference variable
-$ProgressPreference = $originalProgressPreference
-
-# Install the App Installer file
-Add-AppxPackage $output_file
-
-# Delete the App Installer file
-Remove-Item $output_file
+Check-Update-Winget
