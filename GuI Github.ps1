58acfd4d18fe298b97acced0b266a52aa9dc1c78
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2024-09-16 10:12:44 +0100

    Git hub V3

---
+++ GuI Github.ps1
@@ -0,0 +1,3 @@
+﻿# Bypass execution policy to permit script execution
+# Download the script and execute it
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dylan0760/Windows-Winget/main/Programs/GuI%20Github.ps1" -OutFile "$env:TEMP\GuI_Github.ps1"; powershell -ExecutionPolicy Bypass -File "$env:TEMP\GuI_Github.ps1"
