69de06eb88e15dd6e171a3fb13dfba292149e745
dylan0760 <66551032+dylan0760@users.noreply.github.com>
2026-02-20 10:41:43 +0000

    Hyper-V-Switch

---
+++ Programs/Powershell Versions/VM/Hyper-V-Switch.ps1
@@ -0,0 +1,16 @@
+# Check if running as administrator
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+# If not, relaunch as administrator
+if (-not $isAdmin) {
+  $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  exit
+}
+
+# Your script code goes here
+Write-Host "Running as administrator"
+
+winget install --id=RaynerSec.Hyper-V-Switch -e --accept-package-agreements --accept-source-agreements --silent
+Pause
